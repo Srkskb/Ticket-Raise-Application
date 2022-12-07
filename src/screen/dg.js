4aecbee58ba1e340409fc2049514194f5d2edce5
Shaharukh Khan <82607480+Srkskb@users.noreply.github.com>
2022-12-07 11:47:41 +0530

    changes

--- src/screen/dg.js
+++
@@ -1,7 +0,0 @@
-<View style={{paddingTop:width*0.05,}}>
-<View style={{justifyContent:'center',alignItems:'center'}}>
-<Text  style={{fontSize:width*0.04,fontWeight:'500'}}>
-
-</Text>
-</View>
-</View>
